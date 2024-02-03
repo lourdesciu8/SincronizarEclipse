d8e5cfde9b0e209979e3e05c3d24923179708a32
ciu8 <lourdesciu8@gmail.com>
2024-02-03 16:49:51 +0100

    Commit

--- eclipse-workspace/ejemploMetodos/src/concesionario/principalConcesionario.java
+++ eclipse-workspace/ejemploMetodos/src/concesionario/principalConcesionario.java
@@ -30,57 +30,109 @@ public class principalConcesionario {
 	    */
 
 	    do {
-			System.out.println("1.- Dar de alta a un cliente");
-			System.out.println("2.- Dar de baja a un cliente");
-			System.out.println("3.- Dar de alta un vehículo");
-			System.out.println("4.- Dar de baja un vehículo");
-			System.out.println("5.- Salir");
+	    	mostrarMenu();
 			opcion=sc.nextInt();
 			switch(opcion) {
 				case 1: System.out.println("Dime dni del cliente: ");
 						dni=sc.next();
-						System.out.println("Dime el nombre: ");
-						nombre=sc.next();
-						System.out.println("Dime los apellidos: ");
-						apellidos=sc.next();
-						c1=new clientes(dni,nombre, apellidos);
-						clientesConcesionario.add(c1);
-						System.out.println("Cliente dado de alta");
+						boolean existecliente=false;
+						for(clientes c2: clientesConcesionario) {
+					    	if(c2.getDni().equals(dni)) {
+								System.out.println("No se puede dar de alta. El cliente ya está en la base de datos");
+								existecliente=true;
+								break;
+							}
+						}
+						if(!existecliente) {
+							System.out.println("Dime el nombre: ");
+							nombre=sc.next();
+							System.out.println("Dime los apellidos: ");
+							apellidos=sc.nextLine();
+							sc.nextLine();
+							c1=new clientes(dni,nombre, apellidos);
+							clientesConcesionario.add(c1);
+							System.out.println("Cliente dado de alta");
+						}
 						
 					break;
+								
 				case 2: System.out.println("Dime dni del cliente: ");
 						dni=sc.next();
-						for(clientes c1: clientesConcesionario) {
-					    	if(c1.getDni().equals("53736069L")) {
-					    		clientesConcesionario.remove(c1);
+						for(clientes c2: clientesConcesionario) {
+					    	if(c2.getDni().equals(dni)) {
+					    		clientesConcesionario.remove(c2);
 					    		System.out.println("Cliente eliminado");
+					    		break;
+						    }else {
+						    	System.out.println("Cliente no eliminado, no se encuentra guardado");
 						    }
 					    }
 					break;
+					
+				case 3:  System.out.println("Clientes actuales guardados: ");
+				for(clientes c2:clientesConcesionario) {
+					System.out.println(c2.getNombre()+" " +"con DNI "+c2.getDni());
+				}
+				break;
 						
-				case 3: System.out.println("Dime matrícula del vehículo: ");
+				case 4: System.out.println("Dime matrícula del vehículo: ");
 				matricula=sc.next();
+				boolean existevehiculo=false;
+				for(vehiculos c2: vehiculosConcesionario) {
+			    	if(c2.getMatrícula().equals(matricula)) {
+						System.out.println("No se puede dar de alta. El vehículo ya está en la base de datos");
+						existevehiculo=true;
+						break;
+					}
+				}
+				
+				if(!existevehiculo) {
 				System.out.println("Dime marca del vehículo: ");
 				marca=sc.next();
 				v1=new vehiculos(matricula, marca);
 				vehiculosConcesionario.add(v1);
 				System.out.println("Vehículo dado de alta");
+				}
 				
 					break;
-				case 4: System.out.println("Dime matrícula del vehículo: ");
+				case 5: System.out.println("Dime matrícula del vehículo: ");
 				matricula=sc.next();
-				for(vehiculos v1: vehiculosConcesionario) {
-			    	if(v1.getMatrícula().equals("8974JKG")) {
-			    		vehiculosConcesionario.remove(v1);
+				for(vehiculos v2: vehiculosConcesionario) {
+			    	if(v2.getMatrícula().equals(matricula)) {
+			    		vehiculosConcesionario.remove(v2);
 			    		System.out.println("Vehículo eliminado");
+			    		break;
+				    }else {
+				    	System.out.println("Vehículo no eliminado, no se encuentra guardado");
 				    }
 			    }
 			break;
 			
-				case 5: System.out.println("Salir");
+				case 6:  System.out.println("Vehículos actuales guardados: ");
+				for(vehiculos v2:vehiculosConcesionario) {
+					System.out.println(v2.getMarca()+" " +"con matrícula "+v2.getMatrícula());
+				}
+				break;
+			
+				case 7: System.out.println("Salir");
+				break;
+				
+			default:  System.out.println("Opción no válida");
+				
 			}
-		}while (opcion!=5);
+		}while (opcion!=7);
 
 	}
-
-}
+		
+	//Todos los métodos que se encuentre dentro de la clase principal tienen que llevar static
+		public static void mostrarMenu() {
+			System.out.println("Menú de opciones");
+			System.out.println("1.- Dar de alta a un cliente");
+			System.out.println("2.- Dar de baja a un cliente");
+			System.out.println("3.- Mostrar todos los clientes guardados");
+			System.out.println("4.- Dar de alta un vehículo");
+			System.out.println("5.- Dar de baja un vehículo");
+			System.out.println("6.- Mostrar todos los vehículos guardados");
+			System.out.println("7.- Salir");
+			}
+	}	

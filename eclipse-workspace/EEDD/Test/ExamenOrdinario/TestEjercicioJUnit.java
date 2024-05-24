eac177d03868e8f19f4c5f8faca805e1620b871b
ciu8 <lourdesciu8@gmail.com>
2024-05-24 10:48:10 +0200

    commit

--- eclipse-workspace/EEDD/Test/ExamenOrdinario/TestEjercicioJUnit.java
+++ eclipse-workspace/EEDD/Test/ExamenOrdinario/TestEjercicioJUnit.java
@@ -1,5 +1,39 @@
 package ExamenOrdinario;
 
-public class TestEjercicioJUnit {
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+
+@RunWith(Parameterized.class)
+public class TestEjercicioJUnit{
+	//Poner atributos para variable donde se guarda el método que crees
+    
+    
+//Poner constructor de las variables de arriba
+
+@Parameters
+public static Collection<Object[]> datos() {
+	return Arrays.asList(new Object[][] { 
+		{, , , },{ , , , },{ , , , }
+		}
+		);
 
 }
+
+@org.junit.Test //Test
+public void Test() {
+	
+	EjercicioJUnit objeto1=new EjercicioJUnit( , , );
+	double X=objeto1.NombreMetodo( , , ); 
+	assertEquals(CantidadTotal,X);
+	
+	//assertEquals(Valor esperado, Valor real);
+	}
+
+}
+
--- eclipse-workspace/EEDD/src/ExamenOrdinario/EjercicioJUnit.java
+++ eclipse-workspace/EEDD/src/ExamenOrdinario/EjercicioJUnit.java
@@ -1,5 +1,18 @@
 package ExamenOrdinario;
 
 public class EjercicioJUnit {
+	// Atributos
+    
 
+    // Constructores
+   
+
+
+	// Métodos getters y setters 
+    
+
+
+    // Método para calcular 
+   
+	double CantidadTotal;
 }
